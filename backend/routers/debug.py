3804bfebca5427334a2784101ff1c57daf3cfd5c
Pasqualino Floccari <pasqualino.floccari@eng.it>
2026-03-21 00:27:29 +0100

    fix: import HTTPException

--- backend/routers/debug.py
+++ backend/routers/debug.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from sqlalchemy import text
 from database import get_db, get_db_path
